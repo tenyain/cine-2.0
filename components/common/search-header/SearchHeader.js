3eed85435259eb9b7bc5877c8a2ecdb911f2456b
tenyain <tenyain.mhs@gmail.com>
2022-09-03 18:55:39 +0630

    [SearchHeader] updated

--- components/common/search-header/SearchHeader.js
+++ components/common/search-header/SearchHeader.js
@@ -38,16 +38,18 @@ const SearchHeader = () => {
               }}
             />
 
-            <div
-              className={`absolute flex gap-y-1 flex-col z-[500] bg-dark ${
-                displaySuggest ? "visible opacity-100" : " invisible opacity-0"
-              }`}
-            >
-              {suggestData &&
-                suggestData.map((item, index) => {
+            {suggestData && (
+              <div
+                className={`absolute flex gap-y-1 flex-col z-[500] bg-white cus-box-shadow p-1 rounded ${
+                  displaySuggest
+                    ? "visible opacity-100"
+                    : " invisible opacity-0"
+                }`}
+              >
+                {suggestData.map((item, index) => {
                   return (
                     <div
-                      className="rounded cursor-pointer text-secondary px-4 py-3 bg-wah transition-all hover:text-dark hover:bg-wah"
+                      className="rounded cursor-pointer text-secondary border-transparent border border-solid px-4 py-2 bg-wah transition-all hover:text-primary hover:border-primary hover:bg-wah"
                       key={index}
                       onClick={() => handleSuggestion(item)}
                     >
@@ -55,7 +57,8 @@ const SearchHeader = () => {
                     </div>
                   );
                 })}
-            </div>
+              </div>
+            )}
           </form>
         </div>
       </div>
--- pages/upcoming/[pageNo].js
+++ pages/upcoming/[pageNo].js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 /* Components */
 import { SearchHeader } from "../../components/common";
@@ -7,8 +8,57 @@ import { UpcomingPaginatePage } from "../../components";
 const UpcomingPage = () => {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#278EA5" />
+        <meta name="title" content="Upcoming Movies | CINE" />
+        <meta
+          name="description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them."
+        />
+        <meta
+          name="keywords"
+          content="cine, cinema, cinematic, information, movies, series, tv shows, tv, shows, torrents, tmdb, yts, download movies"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="TeNyain Moe Lwin" />
+
+        {/* Primary Meta Tags */}
+        <title>Upcoming Movies | CINE</title>
+        <meta name="title" content="Upcoming Movies | CINE" />
+        <meta
+          name="description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them."
+        />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cine-ent.vercel.app/upcoming/" />
+        <meta property="og:title" content="Upcoming Movies | CINE" />
+        <meta
+          property="og:description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them."
+        />
+        <meta property="og:image" content="/meta.png" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cine-ent.vercel.app/upcoming/" />
+        <meta
+          property="twitter:title"
+          content="Upcoming Movies | CINE"
+        />
+        <meta
+          property="twitter:description"
+          content="A website that provides you cinematic data with stunning UI. Torrents for movies are also available and just a touch to download them."
+        />
+        <meta property="twitter:image" content="/meta.png" />
+      </Head>
       <SearchHeader />
-      <UpcomingPaginatePage/>
+      <UpcomingPaginatePage />
     </>
   );
 };
