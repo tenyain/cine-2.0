a642be7719a97da21c54d4b954bd385fae0c8861
T-N9 <thethoriumtension@gmail.com>
2022-12-30 16:06:18 +0630

    [UpcomingMovies] Link passed

--- components/home/components/upcoming-movies/UpcomingItem.js
+++ components/home/components/upcoming-movies/UpcomingItem.js
@@ -18,11 +18,13 @@ const UpcomingItem = ({ id, title, poster_path, overview, release_date }) => {
         </div>
         <div className="flex-[4] px-3 py-5 flex flex-col justify-between items-start">
           <Link href={`/${route_type}/${id}`}>
-            <div className="cursor-pointer">
-                <h1 className="text-dark inline-block text-[1.2rem] lg:text-xl relative font-heading upcoming-title mb-4">{title}</h1>
-                
-                <p className="text-primary font-bold font-special">{moment(release_date).format("D MMM, YYYY") }</p>
-            </div>
+            <a>
+              <div className="cursor-pointer">
+                  <h1 className="text-dark inline-block text-[1.2rem] lg:text-xl relative font-heading upcoming-title mb-4">{title}</h1>
+              
+                  <p className="text-primary font-bold font-special">{moment(release_date).format("D MMM, YYYY") }</p>
+              </div>
+            </a>
           </Link>
 
           <p className="line-clamp-2 overflow-ellipsis">{overview}</p>
