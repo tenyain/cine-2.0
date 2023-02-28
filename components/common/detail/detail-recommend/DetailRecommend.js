579cd30aff345e2b97c05f7015ac7ac8593a1844
T-N9 <thethoriumtension@gmail.com>
2023-02-28 16:26:13 +0630

    [DetailRecommend] mb updated

--- components/common/detail/detail-recommend/DetailRecommend.js
+++ components/common/detail/detail-recommend/DetailRecommend.js
@@ -9,7 +9,7 @@ const DetailRecommend = ({ media_type, id }) => {
   const { data, error, recommendList } = Hook(media_type, id);
 
   return (
-    <section className="">
+    <section className="mb-12">
       <div className="container_x_md py-5">
         <h1 className="title-2 text-wah mb-5">
           Recommended
