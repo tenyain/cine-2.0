ce77de8ead76fef668968bc0bfa0e06a7da19e4a
T-N9 <thethoriumtension@gmail.com>
2023-03-08 12:25:50 +0630

    [MovieDetail] server test

--- pages/movies/[movieId].js
+++ pages/movies/[movieId].js
@@ -8,73 +8,62 @@ import { API_KEY } from "../../constants/common";
 /* Components */
 import { MovieDetailPage } from "../../components";
 
-// const MovieDetail = ({ id, movie }) => {
+const MovieDetail = ({ id, movie }) => {
+  const media_type = "movie";
 
-//     // const media_type = 'movie';
+  const title = movie.original_title;
+  const backdrop_path = `https://www.themoviedb.org/t/p/original${movie.backdrop_path}`;
+  const overview = movie.overview;
 
-//     // const title = movie.original_title;
-//     // const backdrop_path = `https://www.themoviedb.org/t/p/original${movie.backdrop_path}`;
-//     // const overview = movie.overview;
-
-//   return (
-//     <>
-//       <Head>
-//         <title>{title}</title>
-//         <meta name="title" content={title} />
-//         <meta name="description" content={overview} />
-//         {/* <!-- Open Graph / Facebook --> */}
-//         <meta property="og:type" content="website" />
-//         <meta
-//           property="og:url"
-//           content={`https://cine.tenyain.com/movies/${movie.id}`}
-//         />
-//         <meta property="og:title" content={title} />
-//         <meta property="og:description" content={overview} />
-//         <meta property="og:image" content={backdrop_path} />
-
-//         {/* <!-- Twitter --> */}
-//         <meta property="twitter:card" content="summary_large_image" />
-//         <meta
-//           property="twitter:url"
-//           content={`https://cine.tenyain.com/movies/${movie.id}`}
-//         />
-//         <meta property="twitter:title" content={title} />
-//         <meta property="twitter:description" content={overview} />
-//         <meta property="twitter:image" content={backdrop_path} />
-//       </Head>
-//       {/* className="mt-[70px] lg:mt-[60px]" */}
-//       <section >
-//         {/* <MovieDetailPage
-//           movieId = {id}
-//         /> */}
-//       </section>
-//     </>
-//   );
-// };
-
-const MovieDetail = () => {
   return (
-    <section>
-      movie
-    </section>
-  )
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={overview} />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://cine.tenyain.com/movies/${movie.id}`}
+        />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={overview} />
+        <meta property="og:image" content={backdrop_path} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://cine.tenyain.com/movies/${movie.id}`}
+        />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={overview} />
+        <meta property="twitter:image" content={backdrop_path} />
+      </Head>
+      {/* className="mt-[70px] lg:mt-[60px]" */}
+      <section>
+        <MovieDetailPage movieId={id} />
+      </section>
+    </>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { movieId } = context.params;
+
+  const getMovie = await Promise.all([
+    fetch(`${MOVIE_DETAIL}${movieId}?api_key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => data),
+  ]);
+
+  return {
+    props: {
+      id: movieId,
+      movie: getMovie,
+    }, // will be passed to the page component as props
+  };
 }
 
-// export async function getServerSideProps(context) {
-//   const { movieId } = context.params;
-
-//   const getMovie = await fetch(`${MOVIE_DETAIL}${movieId}?api_key=${API_KEY}`)
-//     .then((res) => res.json())
-//     .then((data) => data);
-
-//   return {
-//     props: {
-//       id: movieId,
-//       movie: getMovie,
-//     }, // will be passed to the page component as props
-//   };
-// }
-
 export default MovieDetail;
-
-
