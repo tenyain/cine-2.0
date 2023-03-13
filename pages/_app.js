817748b3bf841d947935885dd42978564b30634e
T-N9 <thethoriumtension@gmail.com>
2023-03-13 08:36:51 +0630

    [Nav Footer] added

--- pages/_app.js
+++ pages/_app.js
@@ -1,59 +1,58 @@
-import '../styles/font.css';
-import '../styles/globals.css';
-import '../styles/style.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { StyledEngineProvider } from '@mui/material';
+import "../styles/font.css";
+import "../styles/globals.css";
+import "../styles/style.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { StyledEngineProvider } from "@mui/material";
 
-import { Provider } from 'react-redux';
-import { store } from '../modules';
+import { Provider } from "react-redux";
+import { store } from "../modules";
 
 /* Major components */
-import NavBar from '../components/navigation';
-import Progress from '../components/common/progress-bar/Progress';
-import { useProgressStore } from '../store';
-import { Footer } from '../components';
+import NavBar from "../components/navigation";
+import Progress from "../components/common/progress-bar/Progress";
+import { useProgressStore } from "../store";
+import { Footer } from "../components";
 
 /* Components */
 
 function MyApp({ Component, pageProps }) {
-
   const setIsAnimating = useProgressStore((state) => state.setIsAnimating);
-  const isAnimating = useProgressStore((state)=> state.isAnimating);
-  const router = useRouter(); 
+  const isAnimating = useProgressStore((state) => state.isAnimating);
+  const router = useRouter();
 
   useEffect(() => {
     const handleStart = () => {
       setIsAnimating(true);
-    }
+    };
 
     const handleStop = () => {
       setIsAnimating(false);
-    }
+    };
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleStop);
-    router.events.on('routeChangeError', handleStop);
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
 
     return () => {
-      router.events.off('routeChangeStart', handleStart);
-    router.events.off('routeChangeComplete', handleStop);
-    router.events.off('routeChangeError', handleStop);
-    }
-  }, [router])
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
 
   return (
     <StyledEngineProvider injectFirst>
       <Provider store={store}>
-        <main className='font-primary main-overflow bg-dark min-h-screen'>
-          {/* <NavBar/> */}
-          <Progress isAnimating={isAnimating}/>
+        <main className="font-primary main-overflow bg-dark min-h-screen">
+          <NavBar />
+          <Progress isAnimating={isAnimating} />
           <Component {...pageProps} />
-          {/* <Footer/> */}
+          <Footer />
         </main>
       </Provider>
     </StyledEngineProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
