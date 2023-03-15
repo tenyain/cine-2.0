d9f813055a7bf64d5ed67acd64608b272a116a2c
T-N9 <thethoriumtension@gmail.com>
2023-03-15 08:49:23 +0630

    [comment] removed

--- components/common/detail/detail-torrent/hook.detailTorrent.js
+++ components/common/detail/detail-torrent/hook.detailTorrent.js
@@ -7,15 +7,13 @@ const Hook = (imdbID) => {
   const bluray_type = torrentList.filter((movie) => movie.type === "bluray");
   const web_type = torrentList.filter((movie) => movie.type === "web");
 
-  console.log({data})
-
   return {
     data,
     error,
 
     torrentList,
     bluray_type,
-    web_type
+    web_type,
   };
 };
 
