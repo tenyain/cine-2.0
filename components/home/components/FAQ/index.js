7e79503d51a65b72155f5e9f838d02ff01d376f8
T-N9 <thethoriumtension@gmail.com>
2023-03-17 11:26:59 +0630

    [Faq] added index fixed

--- components/home/components/FAQ/index.js
+++ components/home/components/FAQ/index.js
@@ -7,12 +7,15 @@ import FaqItem from "./FaqItem";
 const FaqSection = () => {
   return (
     <section className="container_x_md py-12 mt-6 bg-popular-bg-sm md:bg-popular-bg-lg bg-popular">
-      <h1 className="title-2 mb-10 text-wah text-center">Frequently asked questions</h1>
+      <h1 className="title-2 mb-10 text-wah text-center">
+        Frequently asked questions
+      </h1>
       <div className="flex flex-col gap-5 w-[97%] lg:w-1/2 mx-auto">
-        {faqlist?.map((item, key) => {
+        {faqlist?.map((item, index) => {
           return (
             <FaqItem
-              index={key}
+              key={index}
+              index={index}
               question={item.question}
               answer={item.answer}
             />
