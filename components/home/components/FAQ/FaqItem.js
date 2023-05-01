15dd117ee1111f859564810986da1b045517acb2
T-N9 <thethoriumtension@gmail.com>
2023-05-01 15:41:33 +0630

    [Faq] item bg updated

--- components/home/components/FAQ/FaqItem.js
+++ components/home/components/FAQ/FaqItem.js
@@ -4,7 +4,7 @@ const FaqItem = ({ index, question, answer }) => {
   return (
     <div
       tabIndex={index}
-      className="collapse collapse-arrow bg-base-100 rounded-box  border-4 border-solid border-light text-wah"
+      className="collapse collapse-arrow bg-black rounded-box  border-4 border-solid border-light text-wah"
     >
       <div className="collapse-title text-xl font-medium font-heading">{question}</div>
       <div className="collapse-content">
