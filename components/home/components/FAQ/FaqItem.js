b5871859f96d23404fc277d14b4e98743053eb04
T-N9 <thethoriumtension@gmail.com>
2023-05-29 08:56:11 +0630

    [FAQ] accordion updated

--- components/home/components/FAQ/FaqItem.js
+++ components/home/components/FAQ/FaqItem.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const FaqItem = ({ index, question, answer }) => {
+const Accordion = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const accordionRef = useRef(null);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (accordionRef.current && !accordionRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div
-      tabIndex={index}
-      className="collapse collapse-arrow bg-black rounded-box  border-4 border-solid border-light text-wah"
+      className="border-4 border-solid bg-black border-light rounded-2xl"
+      ref={accordionRef}
     >
-      <div className="collapse-title text-xl font-medium font-heading">{question}</div>
-      <div className="collapse-content">
-        <p>{answer}</p>
+      <div
+        className="flex items-center justify-between p-4 cursor-pointer"
+        onClick={toggleAccordion}
+      >
+        <div className="text-lg font-medium text-wah">{question}</div>
+        <svg
+          className={`w-6 h-6 transition-transform duration-300 transform ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M19 9l-7 7-7-7" />
+        </svg>
       </div>
+      {isOpen && (
+        <div className="p-4 border-t border-gray-300">
+          <p className="text-wah font-primary">{answer}</p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default FaqItem;
+export default Accordion;
