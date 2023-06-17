b18925f23972280aed74069644e359683c41a30c
T-N9 <thethoriumtension@gmail.com>
2023-06-17 20:34:05 +0630

    [SearchInput] updated

--- components/home/components/search-input/SearchInput.js
+++ components/home/components/search-input/SearchInput.js
@@ -4,7 +4,8 @@ import React from "react";
 import Hook from "./hook.searchInput";
 
 /* Components */
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const SearchInput = () => {
   const {
@@ -17,21 +18,24 @@ const SearchInput = () => {
   } = Hook();
 
   return (
-    <section className="py-12 bg-gradient-to-r from-secondary to-primary">
+    <section className="py-14 bg-secondary">
       <div className="container_x_md">
         <div className="flex justify-center items-center">
           <div className="w-full md:w-4/5 lg:w-1/2 flex gap-y-3 flex-col">
-            <h1 className="text-white font-heading font-bold">Explore Now</h1>
-            <form onSubmit={enterHandler}>
+            <h1 className="text-white font-special font-bold">Explore Now</h1>
+            <form onSubmit={enterHandler} className="flex relative">
               <TextField
                 fullWidth
-                label="Search for movies and series"
+                label="Movies or Series"
                 id="fullWidth"
                 autoComplete="off"
                 value={formValue}
                 variant="outlined"
                 onChange={changeHandler}
               />
+              <Button type="submit" variant="contained mr-auto" className="btn-search absolute right-2 top-1/2 -translate-y-1/2">
+                Search
+              </Button>
             </form>
           </div>
         </div>
