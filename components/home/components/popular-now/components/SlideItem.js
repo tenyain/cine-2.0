2c21047f655cb13ef427ca2c4242144dc393730a
T-N9 <thethoriumtension@gmail.com>
2023-06-17 21:11:23 +0630

    [SlideItem] popular now updated

--- components/home/components/popular-now/components/SlideItem.js
+++ components/home/components/popular-now/components/SlideItem.js
@@ -26,7 +26,7 @@ const SlideItem = ({
       <div className="w-[200px] relative cursor-pointer select-none">
         <Link href={`${route_type}/${id}`} passHref={true} prefetch>
           <a>
-          <div className="w-[200px] rounded-xl min-h-[295px] mb-1 relative popular-now-shadow border-2 border-solid border-secondary overflow-hidden">
+          <div className="w-[200px] rounded-xl min-h-[295px] mb-1 relative popular-now-shadow border-2 border-solid border-primary overflow-hidden">
               <Image
                 layout="fill"
                 quality="10"
@@ -37,7 +37,7 @@ const SlideItem = ({
             </div>
             <div className="px-3 py-1">
               <h1 className="text-[18px]">{title ? title : name}</h1>
-              <p className="text-sm my-1">{moment(release_date).format("D MMM, YYYY") }</p>
+              <p className="text-xs my-1 text-gray ">{moment(release_date).format("D MMM, YYYY") }</p>
               <p style={{ 'borderColor': vote_border }} className="absolute -top-4 -right-4 w-10 h-10 border-2 border-solid rounded-full font-bold bg-dark flex justify-center items-center">
                 {vote_avg === 0 ? "NR" : vote_avg.toFixed(1)}
               </p>
